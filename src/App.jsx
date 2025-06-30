5a4d26fdc0fa5494c307cd4f0ee4e26b73ab0a7c
nelsonortfan <n.ortizf@uniandes.edu.co>
2025-06-29 21:56:13 -0500

    Agregando routing al proyecto

--- src/App.jsx
+++ src/App.jsx
@@ -2,15 +2,23 @@ import './App.css'
 import FooterComponent from './components/FooterComponent'
 import HeaderComponent from './components/HeaderComponent'
 import ListEmployeeComponent from './components/ListEmployeeComponent'
+import {BrowserRouter, Route, Routes} from 'react-router-dom';
 
 function App() {
 
 
   return (
     <>
-      <HeaderComponent/>
-      <ListEmployeeComponent/>
-      <FooterComponent/>
+      <BrowserRouter>
+        <HeaderComponent/>
+          <Routes>
+             {/* // http://localhost:3000 */}
+            <Route path='/' element = {<ListEmployeeComponent />} ></Route> 
+            {/* // http://localhost:3000/employees */}
+            <Route path='/employees' element = {<ListEmployeeComponent />} ></Route> 
+          </Routes>
+        <FooterComponent/>
+      </BrowserRouter>  
     </>
   )
 }
