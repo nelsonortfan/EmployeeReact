74dc52bdbc2668390abd2561835737e1519435b1
nelsonortfan <n.ortizf@uniandes.edu.co>
2025-06-30 10:09:18 -0500

    Agregando componente EmployeeComponent y boton para agregar un Employee

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import './App.css'
+import EmployeeComponent from './components/EmployeeComponent';
 import FooterComponent from './components/FooterComponent'
 import HeaderComponent from './components/HeaderComponent'
 import ListEmployeeComponent from './components/ListEmployeeComponent'
@@ -15,7 +16,9 @@ function App() {
              {/* // http://localhost:3000 */}
             <Route path='/' element = {<ListEmployeeComponent />} ></Route> 
             {/* // http://localhost:3000/employees */}
-            <Route path='/employees' element = {<ListEmployeeComponent />} ></Route> 
+            <Route path='/employees' element = {<ListEmployeeComponent />} ></Route>
+            {/* // http://localhost:3000/add-employee */}
+            <Route path='/add-employee' element = {<EmployeeComponent />} ></Route> 
           </Routes>
         <FooterComponent/>
       </BrowserRouter>  
