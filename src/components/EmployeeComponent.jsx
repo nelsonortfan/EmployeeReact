02aff50f02ece1b6b97f5c78f76c69ce05fe7e6c
nelsonortfan <n.ortizf@uniandes.edu.co>
2025-07-04 20:03:07 -0500

    Control del titulo del formulario para agregar o actualizar

--- src/components/EmployeeComponent.jsx
+++ src/components/EmployeeComponent.jsx
@@ -1,12 +1,13 @@
 import React, { useState } from 'react'
 import { createEmployee } from '../services/EmployeeService'
-import { useNavigate } from 'react-router-dom'
+import { useNavigate, useParams } from 'react-router-dom'
 
 const EmployeeComponent = () => {
 
     const [firstName, setFirstName] = useState('')
     const [lastName, setLastName] = useState('')
     const [email, setEmail] = useState('')
+    const {id} = useParams();
 
     const [errors, setErrors] = useState({
         firstName: '',
@@ -63,12 +64,23 @@ const EmployeeComponent = () => {
 
     }
 
+    function pageTitle(){
+        if(id){
+            return <h2 className='text-center'>Update Employee</h2>
+        }
+        else{
+            return <h2 className='text-center'>Add Employee</h2>
+        }
+    }
+
   return (
     <div className='container'>
         <br></br>
         <div className='row'>
             <div className='card col-md-6 offset-md-3 offset-md-3'>
-                <h2 className='text-center'>Add Employee</h2>
+                {
+                    pageTitle()
+                }
                 <div className='card-body'>
                     <form>
                         <div className='form-group mb-2'>
