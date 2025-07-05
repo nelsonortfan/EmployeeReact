72efe94e71ee3eedb84bc4cc5cfeea2b3897d510
nelsonortfan <n.ortizf@uniandes.edu.co>
2025-07-04 19:56:32 -0500

    Agregando router para editar y funcion navigator para saltar al componente Employee

--- src/components/ListEmployeeComponent.jsx
+++ src/components/ListEmployeeComponent.jsx
@@ -20,6 +20,10 @@ const ListEmployeeComponent = () => {
         navigator('/add-employee')
     }
 
+    function updateEmployee(id){
+        navigator(`/edit-employee/${id}`)
+    }
+
   return (
     <div className='container'>
         <h2 className='text-center'>List of Employees</h2>
